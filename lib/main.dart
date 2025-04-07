67453d713794591f33adbd62be902902d1fbeef1
Yash Dhavde <yashdhavde1511@gmail.com>
2025-04-07 18:20:51 +0530

    Readme updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 //Set API key below before building
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ThemeManager.initialize();
